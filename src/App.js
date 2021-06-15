2eb01fb5bf2757b59f62d88223c41af86d6350ad
linkclean <linkclean@naver.com>
2021-06-16 08:06:55 +0900

    useState
    Increment, Decrement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React, {useState} from "react";
 
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
   return (
     <div className="App">
-      HI
+      <h1>useState</h1>
+      <h1>{item}</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
 }
+
+// class App extends React.component{
+//   state = {
+//     item: 1
+//   }
+//   render() {
+//     const {item} = this.state;
+//     return (
+//       <div className="App">
+//       <h1>{item}</h1>
+//       <button onClick={this.incrementItem}>Increment</button>
+//       <button onClick={this.decrementItem}>Decrement</button>
+//     </div>
+//     )
+    
+//   }
+//   incrementItem = () => {
+//     this.setState(state => {
+//       return{
+//         item: state.item + 1
+//       }
+//     })
+//   }
+//   decrementItem = () => {
+//     this.setState(state => {
+//       return{
+//         item: state.item - 1
+//       }
+//     })
+//   }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import UseInput from './useInput/UseInput';
+import UseTabs from './useTabs/UseTabs';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <UseInput />
+    <UseTabs />
   </React.StrictMode>,
   document.getElementById('root')
 );
