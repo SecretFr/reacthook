b05c4e24e52b4606c9135e9345c41f0e2993b170
linkclean <linkclean@naver.com>
2021-06-17 06:44:26 +0900

    add component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import UseInput from './useInput/UseInput';
 import UseTabs from './useTabs/UseTabs';
+import UseEffect from './useEffect/UseEffect';
+import UseTitle from './useTitle/UseTitle';
+import UseClick from './useClick/UseClick';
+import UseConfirm from './useConfirm/UseConfirm';
+import UsePreventLeave from './usePreventLeave/UsePreventLeave';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <UseInput />
     <UseTabs />
+    <UseEffect />
+    <UseTitle />
+    <UseClick />
+    <UseConfirm />
+    <UsePreventLeave/>
   </React.StrictMode>,
   document.getElementById('root')
 );
